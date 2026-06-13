fe6988e8722f4a0c27f22cb065ca52b87541fc77
Thiago Gonnet <thiagogonnet2710@gmail.com>
2026-06-13 09:53:42 -0300

    Agrega @Output y hace two way data binding de quantity

--- src/app/input-integer/input-integer.ts
+++ src/app/input-integer/input-integer.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Concert } from '../interfaces/Concert';
 
 @Component({
@@ -12,17 +12,22 @@ export class InputInteger {
   constructor () { }
   @Input() quantity!: number;
   @Input() max!: number;
+
+  @Output() quantityChange: EventEmitter<number> = new EventEmitter<number>();
    decrementarCantidad() {
     if (this.quantity > 0) {
       this.quantity--;
+      this.quantityChange.emit(this.quantity);
     }
   }
   incrementarCantidad() {
     if (this.quantity < this.max && this.max > 0) {
       this.quantity++;
+      this.quantityChange.emit(this.quantity);
     }
   }
   onChangeQuantity(event: Event): void {
     console.log(event);
+    this.quantityChange.emit(this.quantity);
   }
  }
